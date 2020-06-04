de6fea2427dbe19809e9b21c928525d2b558f959
andersongames <ander.cal45@gmail.com>
2020-06-04 14:36:49 -0300

    routes
    / and create-point

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const server = express()
 
+//configurar pasta pública
+server.use(express.static("public"))
+
 //configurar caminhos  da aplicação
 //página inicial
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
+})
+
+//create-point
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
 })
 
 //ligar o servidor
