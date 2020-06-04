6532b8504f8258d9b893b400fef069c6574d1a8a
andersongames <ander.cal45@gmail.com>
2020-06-04 15:33:50 -0300

    route /search

--- src/server.js
+++ src/server.js
@@ -22,5 +22,10 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 
+//search-results
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
 //ligar o servidor
 server.listen(3000)
