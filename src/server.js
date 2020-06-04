f8eaf0cc72240a3a823ea74d74618418525f735e
andersongames <ander.cal45@gmail.com>
2020-06-04 14:54:23 -0300

    nunjucks

--- src/server.js
+++ src/server.js
@@ -4,15 +4,22 @@ const server = express()
 //configurar pasta pública
 server.use(express.static("public"))
 
+//utilizando template engines
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //configurar caminhos  da aplicação
 //página inicial
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 //create-point
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
 //ligar o servidor
