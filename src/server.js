688c466791581de13b074e2410c5e3ca8ee0f03d
andersongames <ander.cal45@gmail.com>
2020-06-05 11:29:59 -0300

    search db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+//importar o db
+const db = require("./database/db")
+
 //configurar pasta pública
 server.use(express.static("public"))
 
@@ -24,7 +27,21 @@ server.get("/create-point", (req, res) => {
 
 //search-results
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    //pegar os dados ddo banco de dados
+    db.all(`SELECT * FROM places`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+        
+        //mostrar a página html com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total: total })
+    })
 })
 
 //ligar o servidor
