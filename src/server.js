0e870d422f9b212ab7451f2b86f5e3824dc47bbc
andersongames <ander.cal45@gmail.com>
2020-06-05 12:24:24 -0300

    server submit

--- src/server.js
+++ src/server.js
@@ -38,9 +38,40 @@ server.post("/savepoint", (req, res) => {
     //console.log(req.body)
 
     //inserir dados no banco de dados
-    
+    //2. inserir dados na tabela
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);
+`
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+]
 
-    return res.send("ok")
+    function afterInsertData(err) {
+        if(err) {
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.send("ok")
+    }
+
+    db.run(query, values, afterInsertData)
 })
 
 //search-results
