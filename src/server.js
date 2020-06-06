7b05bf6265eaaddbe3381ad54303baf6ba3aa0e5
andersongames <ander.cal45@gmail.com>
2020-06-06 19:05:14 -0300

    ..

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const db = require("./database/db")
 //configurar pasta pública
 server.use(express.static("public"))
 
-//configurar as rotas
+//configurar as rotas/
 server.use(routes)
 /*
 //habilitar o uso do req.body
